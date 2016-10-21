e7478eb0696da4d1f79317f2b8639c96d50e4df0
Jaehyuk Choi <Home@Jaehyuks-MacBook-Air.local>
2016-10-21 10:37:45 +0800

    Factor out repeated evaluation
    d1 and d2 to be evaluated only once

--- BlackScholes/bsprice.R
+++ BlackScholes/bsprice.R
@@ -14,28 +14,28 @@ bsprice <- function(type, s, k, T, r, b, sigma){
     #        delta of the option
     #------------------------------------------------
     #------------------------------------------------
-
-    d1 <- (log(s/k)+(b+0.5*sigma^2)*T)/(sigma*sqrt(T))
-    d2 <- d1 - sigma*sqrt(T)
+    
+    stdev <- sigma*sqrt(T)
+    d1 <- (log(s/k)+(b+0.5*sigma^2)*T) / stdev
+    d2 <- d1 - stdev
+  
+    pnorm.d1 <- pnorm(d1)
+    pnorm.d2 <- pnorm(d2)
+      
     if (type == "call" ){
-        price <- exp((b-r)*T)*s*pnorm(d1)-k*exp(-r*T)*pnorm(d2)
-        delta <- exp((b-r)*T)*pnorm(d1)
-    }else{
-        if (type == "put"){
-            price <- k*exp(-r*T)*pnorm(-d2)-exp((b-r)*T)*s*pnorm(-d1)
-            delta <- exp((b-r)*T)*pnorm(d1)-1
-        }else{
-            if (type == "straddle"){
-                price <- exp((b-r)*T)*s*pnorm(d1)-k*exp(-r*T)*pnorm(d2)+k*exp(-r*T)*pnorm(-d2)-exp((b-r)*T)*s*pnorm(-d1)
-                delta <- 2*exp((b-r)*T)*pnorm(d1)-1
-            }else{
-                if (type == "digit"){
-                    price <- exp(-r*T)*pnorm(d2)*1
-                    delta <- exp(-r*T)/sqrt(2*pi)*exp(-d2^2/2)/(s*sigma*sqrt(T))
-                }
-            }
-        }
+        price <- exp((b-r)*T)*s*pnorm.d1 - k*exp(-r*T)*pnorm.d2
+        delta <- exp((b-r)*T)*pnorm.d1
+    }else if (type == "put"){
+        price <- k*exp(-r*T)*(1-pnorm.d2) - exp((b-r)*T)*s*(1-pnorm.d1)
+        delta <- exp((b-r)*T)*pnorm.d1 - 1
+    }else if (type == "straddle"){
+        price <- exp((b-r)*T)*s*pnorm.d1 - k*exp(-r*T)*pnorm.d2 + k*exp(-r*T)*pnorm(-d2) - exp((b-r)*T)*s*pnorm(-d1)
+        delta <- 2*exp((b-r)*T)*pnorm.d1 - 1
+    }else if (type == "digit"){
+        price <- exp(-r*T)*pnorm.d2*1
+        delta <- exp(-r*T)/sqrt(2*pi)*exp(-d2^2/2)/(s*stdev)
     }
+    
     return(c(price=price,delta=delta))
 }
 
