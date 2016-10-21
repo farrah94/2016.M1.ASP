bde90662bceeb8c10b27a2dac18012ce465dd0e5
Jaehyuk Choi <github@jae.eml.cc>
2016-10-21 15:57:42 +0800

    Added Normla test

--- BlackScholes/test.R
+++ BlackScholes/test.R
@@ -1,4 +1,4 @@
-## Testing BSM price & implied vol functions
+#### Testing BSM price & implied vol functions
 
 spot <- 100
 strike <- seq(80,125,5)
@@ -13,4 +13,20 @@ r <- 0.05
 ( impvol <- CalcBsmImpvol(price=price, spot=spot, strike=strike, t.exp=t.exp, r=r) )
 
 
+
+#### Testing Normal price & implied vol functions
+
+spot <- 100
+strike <- seq(80,125,5)
+strike <- 95
+t.exp <- 1.2
+sigma <- 20
+r <- 0.1
+
+# Compute Price on different strikes
+( price <- CalcNormalPrice(spot=spot, t.exp = t.exp, sigma=sigma, strike=strike, r=r) )
+#( price <- 1/sqrt(2*pi)*sigma*sqrt(t.exp) ) ## ATM case
+
+# Compute implied volatility
+( impvol <- CalcNormalImpvol(price=price, spot=spot, strike=strike, t.exp=t.exp, r=r) )
 
