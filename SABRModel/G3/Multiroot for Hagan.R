902d05d1f42cc5589067f4e4b39452d1755dade6
jumetzler <julian_metzler@freenet.de>
2016-11-05 22:14:20 +0800

    Generalise the strikes

--- SABRModel/G3/Multiroot for Hagan.R
+++ SABRModel/G3/Multiroot for Hagan.R
@@ -6,11 +6,11 @@ sigma.alpha.rho <- function(forward, strike, t.exp){
   sub <- function(x){
     sigma <- phbsasp::CalcNormalImpvolSabrHagan(forward=spot, strike = strike, t.exp = t.exp, sigma0 = sigma0, alpha = alpha, rho = rho)
     f1 <- phbsasp:::CalcNormalImpvolSabrHagan(forward=spot, strike = strike[1], t.exp = t.exp, sigma0 = x[1], alpha = x[2], rho = x[3]) - sigma[1]
-    f2 <- phbsasp:::CalcNormalImpvolSabrHagan(forward=spot, strike = strike[2], t.exp = t.exp, sigma0 = x[1], alpha = x[2], rho = x[3]) - sigma[2]
-    f3 <- phbsasp:::CalcNormalImpvolSabrHagan(forward=spot, strike = strike[3], t.exp = t.exp, sigma0 = x[1], alpha = x[2], rho = x[3]) - sigma[3]
+    f2 <- phbsasp:::CalcNormalImpvolSabrHagan(forward=spot, strike = strike[length(strike)/2], t.exp = t.exp, sigma0 = x[1], alpha = x[2], rho = x[3]) - sigma[length(strike)/2]
+    f3 <- phbsasp:::CalcNormalImpvolSabrHagan(forward=spot, strike = strike[length(strike)], t.exp = t.exp, sigma0 = x[1], alpha = x[2], rho = x[3]) - sigma[length(strike)]
     return(c(f1, f2, f3))
   }
   values <- multiroot(f = sub, c(0.6, 0.3, -0.02))$root
-  names(values) <- c('sigma0',"rho",'alpha')
+  names(values) <- c("sigma0","alpha","rho")
   return(values)
 }
